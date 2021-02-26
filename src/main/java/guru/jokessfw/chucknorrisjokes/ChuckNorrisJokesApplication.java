1b440f6d0949eabde00318e8823c4d0f15bf8fd9
rmgooogle <rm.gooogle@gmail.com>
2021-02-26 19:16:00 +0300

    create proj

---
+++ src/main/java/guru/jokessfw/chucknorrisjokes/ChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package guru.jokessfw.chucknorrisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesApplication.class, args);
+    }
+
+}
